f2192f836e21111128af5a5b4bb5fbfa5ac5b4b1
pvahakan <pauli.vahakangas@gmail.com>
2021-02-02 20:53:25 +0200

    added function to user agent

--- yr_api_test.py
+++ yr_api_test.py
@@ -3,6 +3,12 @@
 import urllib.request
 import json
 
+def read_user_agent(path_to_file):
+    f = open(path_to_file, 'r')
+    str_agent = f.read()
+    f.close()
+    return str_agent
+
 
 def read_weather_data():
     """
@@ -13,7 +19,7 @@ def read_weather_data():
     # Oulu
     request_url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=65.0&lon=25.5'
     # User agent pitää olla, jotta YR antaa dataa, oli joku nettisivu, joka näytti tämän
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+    user_agent = read_user_agent('./user_agent.txt')
     headers = {'User-Agent': user_agent,}
     # Data saadaan ulos näin
     response = urllib.request.Request(request_url, None, headers)
@@ -28,4 +34,5 @@ def test_print():
         print(i)
 
 if __name__ == '__main__':
-    test_print()
+    # test_print()
+    read_weather_data()
